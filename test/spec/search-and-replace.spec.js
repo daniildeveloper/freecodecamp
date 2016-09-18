9cb7ededfaf4b65217ecc97c35e8c2b945dfd8d0
Daniil <daniilborowkow@yandex.ru>
2016-09-18 08:54:25 +0600

    Intermediate alghoritm scripting. search-and-replace completed

--- test/spec/search-and-replace.spec.js
+++ test/spec/search-and-replace.spec.js
@@ -1,13 +1,13 @@
 describe('Perform a search and replace on the sentence using the arguments provided and return the new sentence.', function () {
   it('myReplace("Let us go to the store", "store", "mall") should return "Let us go to the mall".', function () {
-    expect(myReplace("Let us go to the store", "store", "mall")).toEqual("Let us go to the mall");
+    expect(replace("Let us go to the store", "store", "mall")).toEqual("Let us go to the mall");
   });
 
   it('myReplace("He is Sleeping on the couch", "Sleeping", "sitting") should return "He is Sitting on the couch".', function () {
-    expect(myReplace("He is Sleeping on the couch", "Sleeping", "sitting")).toEqual('He is Sitting on the couch');
+    expect(replace("He is Sleeping on the couch", "Sleeping", "sitting")).toEqual('He is Sitting on the couch');
   });
 
   it('myReplace("This has a spellngi error", "spellngi", "spelling") should return "This has a spelling error".', function () {
-    expect(myReplace("This has a spellngi error", "spellngi", "spelling")).toEqual("This has a spelling error");
+    expect(replace("This has a spellngi error", "spellngi", "spelling")).toEqual("This has a spelling error");
   });
 });
