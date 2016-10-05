8660a9f933b94462fcf4dec75f265b0572fdb5df
daniiltserin <daniilborowkow@yandex.ru>
2016-10-05 17:12:05 +0600

    Bonfire spinal tap case

--- inermediate-alghoritm-scripting-js/sorted-union.js
+++ inermediate-alghoritm-scripting-js/sorted-union.js
@@ -3,11 +3,27 @@
  *  In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
  * The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
  * Check the assertion tests for examples.
+ *
  */
 function uniteUnique(arr) {
-    return arr;
-}
+    //create an empty array to store result data
+    var res = [];
 
-uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+    //loop throught arguments
+    for (var i = 0; i < arguments.length; i++) {
+        var argumentsOfArray = arguments[i];
+
+        //loops throught the array at hand
+        for (var j = 0; j < argumentsOfArray.length; j++) {
+            var indexV = argumentsOfArray[j];
+            //check if the value is already on the final array
+            if (res.indexOf(indexV) < 0) {
+                res.push(indexV);
+            }
+        }
+    }
+
+    return res;
+}
 
 
